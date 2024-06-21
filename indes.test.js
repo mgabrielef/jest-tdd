1ad4d0d335b95816e57add0ae50f19820fe22729
Gabriele Ferreira <mariagabrieleoliveira@gmail.com>
2024-06-21 18:07:49 -0300

    jest course start

---
+++ indes.test.js
@@ -0,0 +1,13 @@
+const {add, double} = require('./index')
+
+describe('math functions', () => {
+    it('should add two values', () => {
+        expect(add(2,5)).toBe(7)
+        expect(add(3,1)).toBe(4)
+        expect(add(9,4)).toBe(13)
+    });
+
+    it('should double the value', () => {
+        expect(double(3)).toBe(6)
+    });
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+const add = (a,b) => a+b
+const double = x => x*2
+
+module.exports = {add, double}
